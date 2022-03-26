169244c2ba3f2a0ddf91221be2395aad1b4a4eef
Buntu Cossie <buntucossie@gmail.com>
2022-03-26 21:35:32 +0200

    Improved animations

--- src/Components/About/about.tsx
+++ src/Components/About/about.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion, Variants } from 'framer-motion'
 import { useEffect, useState } from 'react'
 import { containerVariant } from '../../variants'
 import './about.css'
@@ -27,6 +27,29 @@ const aboutNavBtns = [
     "Misc"
 ]
 
+const variant: Variants = {
+    start: {
+        x: -50,
+        opacity: 0
+    },
+    end: ({index}) => ({
+        opacity: 1,
+        x: 0,
+        transition: {
+            ease: 'easeOut',
+            delay: index * 0.1
+        }
+    }),
+    exit: ({reverse}) => ({
+        opacity: 0,
+        x: -50,
+        transition: {
+            ease: 'easeInOut',
+            delay: reverse * 0.05
+        }
+    })
+}
+
 function AboutMain() {
     const [page, setPage] = useState(aboutNavBtns[0])
     return (
@@ -38,7 +61,9 @@ function AboutMain() {
                     </button>)}
             </div>
             <div id="aboutContent">
-               {page == 'About' ? <AboutMe  /> : page == 'Languages' ? <Languages arr={langs} /> : page == 'Front-End' ? <Languages arr={frontend} /> : page == "Back-End" ? <Languages arr={backend} /> : <Languages arr={misc} />}     
+            <AnimatePresence exitBeforeEnter>
+               {page == 'About' ? <AboutMe key="about"  /> : page == 'Languages' ? <Languages arr={langs} key="languages" /> : page == 'Front-End' ? <Languages arr={frontend} key="frontend" /> : page == "Back-End" ? <Languages arr={backend} key="backend" /> : <Languages arr={misc} key="misc" />}     
+            </AnimatePresence>
             </div>
         </div>
     )
@@ -46,20 +71,22 @@ function AboutMain() {
 
 function AboutMe() {
     return (
-        <h3>
+        <motion.h3 initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} >
             I am Buntu Cossie, a full-stack developer based in South Africa. Select a tab {window.innerWidth > 768 ? "on the left" : "at the top of the page"} to view my proficiencies.
-        </h3>
+        </motion.h3>
     )
 }
 
 function Languages({arr}: {arr: lang[]}) {
     return (
-        <>
-            {arr.map(lingo => 
-                <div key={lingo.language} className='langDiv'>
+        <>  
+            {/* <AnimatePresence exitBeforeEnter> */}
+            {arr.map((lingo, i) => 
+                <motion.div key={lingo.language} className='langDiv' variants={variant} initial="start" animate="end" exit="exit" custom={{index: i, reverse: arr.length - 1 - i}}  >
                     <img className='langLogos' src={lingo.logo} alt={`${lingo.language} logo`} />
                     <span style={{marginLeft: "1.5rem"}}>{lingo.language}</span>
-                </div>)}
+                </motion.div>)}
+            {/* </AnimatePresence> */}
         </>
     )
 }
--- src/Components/Quotes/quotes.tsx
+++ src/Components/Quotes/quotes.tsx
@@ -1,4 +1,4 @@
-import {  useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import { quotes } from "./quotelist";
 import './quotes.css'
 import { motion } from "framer-motion";
