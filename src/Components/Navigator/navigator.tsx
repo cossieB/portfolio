cfdd42292161dde42a11ef1d52984f5264f7e95d
Buntu Cossie <buntucossie@gmail.com>
2022-04-05 19:24:10 +0200

    Made navbar click-away-able

--- src/Components/Navigator/navigator.tsx
+++ src/Components/Navigator/navigator.tsx
@@ -1,25 +1,25 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import './navigator.css'
 
 
-interface O { 
-    label: string, 
-    path: string 
+interface O {
+    label: string,
+    path: string
 }
 
 const paths: O[] = [
     {
         label: "Home",
         path: "/"
-    }, 
+    },
     {
         label: "About Me",
         path: "/about"
     }, {
         label: "Contact Me",
         path: "contact"
-    },{
+    }, {
         label: "Calculator",
         path: "/calculator"
     }, {
@@ -48,22 +48,46 @@ const paths: O[] = [
 export default function RouteNavigator() {
     const navWrapper = useRef<HTMLElement>(null)
     const btnWrapper = useRef<HTMLButtonElement>(null)
+    const [isOpen, setIsOpen] = useState(navWrapper.current?.classList.contains('isClosed'))
+
     function handleClick() {
         navWrapper.current?.classList.toggle("isClosed")
         btnWrapper.current?.classList.toggle("isClosed")
         btnWrapper.current?.classList.toggle("isOpen")
+        setIsOpen(prev => !prev)
     }
     return (
         <div>
             <button className="isClosed" ref={btnWrapper} id="menuBtn" onClick={handleClick} >
-                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                </svg>
+                {isOpen ? <span style={{fontSize: 32, color: '#ddd'}}>X</span> :
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                }
             </button>
+            <div id="mask" className={isOpen ? "" : 'isHidden'} onClick={handleClick} ></div>
             <nav className="isClosed" ref={navWrapper} id="navigator">
                 {paths.map(item =>
                     <NavLink activeStyle={{ fontStyle: "italic" }} key={item.path} exact to={item.path}><div className="linkdiv">{item.label}</div></NavLink>
                 )}
+                    <a href="https://mememachine.vercel.app/" target="_blank" rel="noreferrer">
+                <div className="linkdiv">
+                        Meme Machine &nbsp;
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+                            <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+                        </svg>
+                </div>
+                    </a>
+                    <a href="https://cossie.herokuapp.com/" target="_blank" rel="noreferrer">
+                <div className="linkdiv">
+                        Microservices &nbsp;
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z" />
+                            <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z" />
+                        </svg>
+                </div>
+                    </a>
             </nav>
         </div>
     )
