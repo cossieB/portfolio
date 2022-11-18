4fb2b67eb04f7cd4b0b0c2d6abf64aa376934db0
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2022-11-18 18:57:04 +0200

    feat: it moves!!

--- src/Components/2048/2048.tsx
+++ src/Components/2048/2048.tsx
@@ -1,14 +1,21 @@
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styles from './2048.module.scss';
 import { Block } from './Board';
 
 const numbers = new Array(16).fill(0).map((_, i) => i)
 
-export default function Game2048() {
-    type T = (Block & { id: number });
-    const [array, setArray] = useState<T[]>([])
+interface P {
+    top: number
+    left: number
+    value: number
+    id: number
+    index: number
+}
 
+export default function Game2048() {
+    const [array, setArray] = useState<P[]>([])
+    const [render, rerender] = useState(false)
     useEffect(() => {
         createBlock(0)
         createBlock(1)
@@ -17,32 +24,65 @@ export default function Game2048() {
     useEffect(() => {
         document.addEventListener('keydown', handleKeyPress)
         return () => document.removeEventListener('keydown', handleKeyPress)
-    }, [])
+    }, [array.length])
 
     function handleKeyPress(e: KeyboardEvent) {
         const validKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'] as const;
         type Key = typeof validKeys[number];
-        if (!validKeys.includes(e.key as any)) return;
-        console.log(e.key)
-    }
+        if (!validKeys.includes(e.key as Key)) return;
+        const key = e.key as Key
 
+        if (key == 'ArrowLeft' || key == 'ArrowRight') {
+            // const factor = key == 'ArrowLeft' ? 1 : -1
+            const sorted = [...array].sort((a, b) => {
+                if (a.left < b.left) return -1
+                else return 1
+            })
+            
+            for (let row = 0; row < 4; row++) {
+                const arr = sorted.filter(x => x.top == row);
+                if (arr.length == 0) continue;
+                arr.forEach((val, i) => {
+                    const item = array.find(x => x.id == val.id)!;
+                    if (key == 'ArrowLeft') 
+                        item.left = i;
+                    else
+                        item.left = 4 - arr.length + i
+                })                
+            }
+        }
+        rerender(prev => !prev)
+    }
+    function move(direction: 'up' | 'down' | 'left' | 'right') {
+        if (direction == 'left' || direction == 'right') {
+            const factor = direction == 'left' ? 1 : -1
+            const sorted = [...array].sort((a, b) => {
+                if (factor * a.left < factor * b.left) return -1
+                else return 1
+            })
+            sorted.forEach(item => {
+
+            })
+        }
+    }
     function createBlock(count?: number) {
         if (array.length == 16) return;
-        const empties = numbers.filter(num => !array.map(t => t.index).includes(num))
+        const empties = numbers.filter(num => !array.map(t => 4 * t.top + t.left ).includes(num))
         const tempIndex = Math.floor(Math.random() * empties.length);
         const index = empties[tempIndex]
-        const id = count || Math.max(...array.map(elem => elem.id)) + 1 
-        const block = new Block(index); 
-        // @ts-expect-error
-        setArray(prev => [...prev, { id, ...block}])
+        const top = Math.floor(index / 4)
+        const left = index % 4
+        const id = count ?? Math.max(...array.map(elem => elem.id)) + 1
+        const value = Math.random() < 0.75 ? 2 : 4;
+        setArray(prev => [...prev, { id, value, top, left, index }])
         console.log(array)
     }
 
     return (
         <div className={styles.container2048}>
-                <button onClick={() => createBlock()}>
-                    Test
-                </button>
+            <button onClick={() => createBlock()}>
+                Test
+            </button>
             <div className={styles.board}>
                 {new Array(16).fill(0).map((_, i) => <div className={styles.block} key={i} />)}
                 <AnimatePresence>
@@ -52,6 +92,8 @@ export default function Game2048() {
                             index={item.index}
                             value={item.value}
                             key={item.id}
+                            top={item.top}
+                            left={item.left}
                         />
                     )}
                 </AnimatePresence>
@@ -60,26 +102,24 @@ export default function Game2048() {
     )
 }
 
-interface P {
-    index: number
-    value: number
-    id: number
-}
+function ControlElem({ top, left, value, id }: P) {
 
-function ControlElem({ index, value, id }: P) {
-    const top = Math.floor(index / 4)
-    const left = index % 4
     const ref = useRef<HTMLDivElement>(null)
 
     useLayoutEffect(() => {
         const div = ref.current!
-        const xFactor = top + .5
         div.style.top = `${top * 7.5 + 0.5 * (top + 1)}rem`
         div.style.left = `${left * 7.5 + 0.5 * (left + 1)}rem`
-    }, [])
+    }, [top, left])
     return (
-        <div ref={ref} className={styles.controlElem}>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{duration: 0.1}}
+            ref={ref}
+            className={styles.controlElem}
+        >
             {value}
-        </div>
+        </motion.div>
     )
 }
--- src/Components/2048/Board.ts
+++ src/Components/2048/Board.ts
@@ -1,32 +1,26 @@
-export default class Board {
-    public array: (Block | null)[] = new Array(16).fill(null)
-
-    constructor() {
-        this.createBlock()
-        this.createBlock()
-    }
-
-    createBlock() {
-        const empties = this.array.filter(item => item == null) 
-        const index = Math.floor(Math.random() * empties.length)
-        this.array[index] = new Block(index)
-        return this.array
-    }
+interface P {
+    top: number
+    left: number
+    value: number
+    id: number
+    index: number
 }
 
-export class Block {
+export class Block implements P {
 
-    public value: number
-    public index: number
+    public left: number;
+    public top: number;
+    public value: number;
+    readonly id: number;
 
-    constructor (index: number) {
-        this.index = index;
+    constructor (left: number, top: number, id: number) {
         this.value = Math.random() < 0.75 ? 2 : 4;
+        this.left = left;
+        this.top = top;
+        this.id = id;
     }
-    top() {
-        return Math.floor(this.index / 4)
+    get index() {
+        return 4 * this.top + this.left
     }
-    left() {
-        return this.index % 4
-    }
+
 }
