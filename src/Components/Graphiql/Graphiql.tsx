62e4ed4faaefbde6a603175b1341784d07912bd6
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-06-13 13:53:41 +0200

    example graphql query

--- src/Components/Graphiql/Graphiql.tsx
+++ src/Components/Graphiql/Graphiql.tsx
@@ -6,13 +6,32 @@ import { useEffect } from 'react';
 
 const fetcher = createGraphiQLFetcher({ url: 'https://internet-games-database.vercel.app/api/graphql' });
 
+const exampleQuery = `#graphql
+query ExampleQuery($gameId: ID!) {
+    game(id: $gameId) {
+        title
+        releaseDate
+        developer {
+            name
+            location
+            country
+        }
+        publisher {
+            name
+            headquarters
+            country
+        }
+    }
+}
+`
+
 export default function GraphiqlPage() {
     useEffect(() => {
         document.title = "GraphiQL"
     })
     return (
         <div style={{height: '100vh'}}>
-            <GraphiQL fetcher={fetcher} />
+            <GraphiQL defaultQuery={exampleQuery} variables={`{"gameId":"2953b48c-36c2-47a2-b3f3-0af9557a2855"}`} fetcher={fetcher} />
         </div>
     )
 }
